5fb4373521d1bc27a32eee6dcc329a0e141ee9b1
Xiang Jiang <xiangj1@outlook.com>
2019-05-11 17:34:01 -0700

    its a begening!

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './model/Header';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+  }
+
+  render() {
+    return (
+      <div className="col-lg-12">
+        <div className="d-flex justify-content-between">
+          {}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/model/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+        This is my own Header
+      </div>
+    );
+  }
+
+}
+
+export default Header;
